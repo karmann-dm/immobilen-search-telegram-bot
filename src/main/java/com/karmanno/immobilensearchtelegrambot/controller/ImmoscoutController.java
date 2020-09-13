496c6184adedad2e656387328ee8e3550223d2e0
karmann-dm <karmanno@gmail.com>
2020-09-13 18:57:10 +0300

    fix: add consumer logging

--- src/main/java/com/karmanno/immobilensearchtelegrambot/controller/ImmoscoutController.java
+++ src/main/java/com/karmanno/immobilensearchtelegrambot/controller/ImmoscoutController.java
@@ -1,6 +1,8 @@
 package com.karmanno.immobilensearchtelegrambot.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import oauth.signpost.OAuthConsumer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/immoscout")
 @Slf4j
+@RequiredArgsConstructor
 public class ImmoscoutController {
+    private final OAuthConsumer oAuthConsumer;
+
     @GetMapping
     public void callbackHandler() {
         log.info("Callback!!!!");
+        String requestToken = oAuthConsumer.getToken();
+        String requestTokenSecret = oAuthConsumer.getTokenSecret();
+        log.info("Token: {}", requestToken);
+        log.info("Token secret: {}", requestTokenSecret);
     }
 }
