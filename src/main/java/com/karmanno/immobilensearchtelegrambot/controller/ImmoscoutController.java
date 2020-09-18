9c5f479c2306b924bbc87c5cf70fd1cdcac66ef9
karmann-dm <karmanno@gmail.com>
2020-09-18 17:06:57 +0300

    fix: logging

--- src/main/java/com/karmanno/immobilensearchtelegrambot/controller/ImmoscoutController.java
+++ src/main/java/com/karmanno/immobilensearchtelegrambot/controller/ImmoscoutController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import oauth.signpost.OAuthConsumer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,11 +16,18 @@ public class ImmoscoutController {
     private final OAuthConsumer oAuthConsumer;
 
     @GetMapping
-    public void callbackHandler() {
+    public void callbackHandler(
+            @RequestParam("oauth_token") String oauthToken,
+            @RequestParam("oauth_verifier") String oauthVerifier,
+            @RequestParam("state") String state
+    ) {
         log.info("Callback!!!!");
         String requestToken = oAuthConsumer.getToken();
         String requestTokenSecret = oAuthConsumer.getTokenSecret();
         log.info("Token: {}", requestToken);
         log.info("Token secret: {}", requestTokenSecret);
+        log.info("OAuth token: {}", oauthToken);
+        log.info("OAuth verifier: {}", oauthVerifier);
+        log.info("State: {}", state);
     }
 }
